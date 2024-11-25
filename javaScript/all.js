9178be8606dc89012bf4ee403f854268fbb82124
unknown <nahidujjamanraj@gmail.com>
2024-11-25 11:23:26 +0600

    In this part i add hidden in font page and remove hidden in keyboard when press button

---
+++ javaScript/all.js
@@ -0,0 +1,5 @@
+function add_hidden(){
+    add_Class_name('home_page')
+    remove_class_name('keyboard_page')
+
+}
---
+++ javaScript/utility.js
@@ -0,0 +1,14 @@
+// add  remove class list 
+function add_Class_name(element){
+    let x= document.getElementById(element);
+    x.classList.add('hidden')
+
+
+}
+
+// kick out remove 
+
+function remove_class_name(element){
+    let y=document.getElementById(element);
+    y.classList.remove('hidden')
+}
