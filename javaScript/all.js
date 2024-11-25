7ae2610a532e4976d2ae6b6a8a1237eb4ab27735
unknown <nahidujjamanraj@gmail.com>
2024-11-25 12:03:28 +0600

    In this program I make the random number and set it in display

--- javaScript/all.js
+++ javaScript/all.js
@@ -1,5 +1,19 @@
-function add_hidden(){
+// continue the game 
+
+function continue_game(){
+    let random_latter=make_random_number();
+
+    let find_random_latter_home=document.getElementById('screen_output')
+    find_random_latter_home.innerText=random_latter;
+
+    set_random_color(random_latter)
+}
+
+// play the game
+function game_start(){
     add_Class_name('home_page')
     remove_class_name('keyboard_page')
+    continue_game()
+    
 
 }
--- javaScript/utility.js
+++ javaScript/utility.js
@@ -12,3 +12,24 @@ function remove_class_name(element){
     let y=document.getElementById(element);
     y.classList.remove('hidden')
 }
+
+// make random color
+
+function set_random_color(element){
+    let x=document.getElementById(element)
+    x.classList.add('bg-orange-500')
+}
+
+// make random number
+
+function make_random_number(){
+
+    let all_latter='abcdefghijklmnopqrstuvwxyz'
+    let separate=all_latter.split('')
+    
+    let make_random_number=Math.round(Math.random()*24);
+    let random_latter=separate[make_random_number]
+    return random_latter
+    
+
+}
